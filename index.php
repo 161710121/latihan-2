af7b864cbeb256fb8517081df5c73c7242d60548
Lab RPL 1 <161710121@smkassalaambandung.sch.id>
2017-10-24 17:30:45 +0400

    guru

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+class guru
+{
+	public $nama, $nip, $mapel;
+
+	public function __construct ($nama, $nip, $mapel)
+	{
+		$this->nama = $nama;
+		$this->nip = $nip;
+		$this->mapel = $mapel;
+	}
+	
+}
+?>
---
+++ tampil.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'index.php';
+
+$a = new guru('Adit', 161710121, 'RPL');
+echo "Nama Guru : ".$a->nama."<br>";
+echo "Nip Guru : ".$a->nip."<br>";
+echo "Mata Pelajaran : ".$a->mapel."<br>";
+?>
